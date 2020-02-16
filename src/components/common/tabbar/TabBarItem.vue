e08bd5995cfd0372ff23e08cb4ad9dba56fee999
sust-mazhen <291540704@qq.com>
2020-02-16 18:40:56 +0800

    Project 框架完成

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'TabBarItem',
+    props:{
+      path: String,
+      activeColor:{
+        type:String,
+        default:"red"
+      }
+    },
+    data() {
+      return {
+        // isActive:false,
+      }
+    },
+    computed:{
+      isActive(){
+        return this.$route.path.indexOf(this.path)!==-1;
+      },
+      activeStyle(){
+        return this.isActive? {color:this.activeColor}:{};
+      }
+    },
+    methods:{
+      itemClick(){
+        if(this.$route.path!==this.path){
+          this.$router.push(this.path);
+          // this.isActive = true;
+        } 
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 10px;
+  }
+  .tab-bar-item img {
+    margin-top: 4px;
+    width: 22px;
+    height: 22px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <tab-bar>
+      <!-- <tab-bar-item path="/home" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item> -->
+      <!-- <tab-bar-item path="/category" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/cart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/cart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="deepPink">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item> -->
+
+      <!--简洁写法-->
+      <tab-bar-item v-for="item in tabbars" :key="item.id" :path="item.path" :activeColor="item.textColor">
+        <img slot="item-icon" :src="item.img" alt="">
+        <img slot="item-icon-active" :src="item.actimg" alt="">
+        <div slot="item-text">{{item.text}}</div>
+      </tab-bar-item>
+
+    </tab-bar>
+
+</template>
+
+<script>
+  import TabBar from "components/common/tabbar/TabBar";
+  import TabBarItem from "components/common/tabbar/TabBarItem";
+  export default {
+    name:'MainTabBar',
+    data(){
+      return {
+          tabbars:[
+            {
+              path:'/home',
+              img:require('assets/img/tabbar/home.svg'),
+              textColor:'deepPink',
+              actimg:require('assets/img/tabbar/home_active.svg'),
+              text:'首页'
+            },
+            {
+              path:'/category',
+              img:require('assets/img/tabbar/category.svg'),
+              textColor:'deepPink',
+              actimg:require('assets/img/tabbar/category_active.svg'),
+              text:'分类'
+            },
+            {
+              path:'/cart',
+              img:require('assets/img/tabbar/cart.svg'),
+              textColor:'deepPink',
+              actimg:require('assets/img/tabbar/cart_active.svg'),
+              text:'购物车'
+            },
+            {
+              path:'/profile',
+              img:require('@/assets/img/tabbar/profile.svg'),
+              textColor:'deepPink',
+              actimg:require('@/assets/img/tabbar/profile_active.svg'),
+              text:'我的'
+            }
+          ],
+
+        }
+    },
+    components:{
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+</style>
