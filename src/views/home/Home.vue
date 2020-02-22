0e9176c0e3dacf5f5d78dccf06eb3ca2e120d852
sust-mazhen <291540704@qq.com>
2020-02-22 23:39:44 +0800

    Project 框架完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,74 @@
 <template>
-  <div>
-     <h2>首页</h2>
+  <div id="home">
+
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
+    <feature-view/>
+    <ul>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+      <li>111111111111</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+
+  import NavBar from 'components/common/navbar/NavBar';
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from './childComps/RecommendView';
+  import FeatureView from "./childComps/FeatureView";
+  import {getHomeMultdata} from "../../network/home";
+
   export default {
-    name:'Home'
+    name:'Home',
+    components:{
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView
+    },
+    data(){
+      return {
+        banners:[],
+        recommend:[]
+      }
+    },
+    created() {
+      // 请求多个数据
+      getHomeMultdata().then(res=>{
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
 </style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="feature">
+      <a href="https://act.mogujie.com/zzlx67">
+        <img src="~assets/img/home/recommend_bg.jpg" alt="123">
+      </a>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "FeatureView"
+  }
+</script>
+
+<style scoped>
+  .feature {
+    border: 2px solid #eeeeee;
+    width: 96%;
+    margin: 5px auto;
+    border-radius: 4px;
+  }
+
+  .feature img {
+    width: 100%;
+  }
+</style>
